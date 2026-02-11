efcdabee14d6493ab4ef85da1664854ca03948dc
Zahin Alwa <zahin.alwa@gmail.com>
2026-02-11 18:30:30 +0530

    [Feature] (/ab/route.js) Converted serverless fn to Edge fn

---
+++ api/ab/route.js
@@ -0,0 +1,27 @@
+export const runtime = "edge";
+
+import { POLICIES, EXPERIMENT } from "../../constants/constants.mjs";
+import { getVariant } from "../../utils/utils.js";
+
+export default function handler(req) {
+  const { searchParams } = new URL(req.url);
+  const siteId = searchParams.get("siteId") || "default";
+  const sessionId = searchParams.get("sessionId") || "anon";
+
+  const variant = getVariant({ siteId, sessionId, EXPERIMENT });
+
+  return new Response(
+    JSON.stringify({
+      EXPERIMENT,
+      variant,
+      policy: POLICIES[variant],
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store",
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+  );
+}
